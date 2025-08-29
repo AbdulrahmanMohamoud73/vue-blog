8916603d198951e6ad060c8b91ae7cabb0b59c0e
axm2293 <axm2293@student.bham.ac.uk>
2025-08-29 13:21:50 +0100

    'edit' post link set up in router

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,31 +6,95 @@ fetchPosts();
 </script>
 
 <template>
-    <div class="wrapper" v-if="postsLoaded">
-        <div class="articleDiv" v-for="(post, id) in allPosts" :key="id" @click="$router.push({ name: 'Post', params: { id: post.id } })">
-          <div class="title">
-            <h2>{{ post.title }}</h2>
-        </div> 
-          <div><p>{{post.description}}</p></div>
-          <div class="timeanddate">
-            <div class="date">{{ new Date(post.published_at).toLocaleDateString('en-GB', {
-              day: 'numeric', month: 'short', year: 'numeric'
-            })}}</div>
-            <div>1 minute read</div>
-          </div>
+  <div class="articles" v-if="postsLoaded">
+    
+    <div class="edit-wrapper">
+      <div class="articleDiv" v-for="(post, id) in allPosts" :key="id"
+        @click="$router.push({ name: 'Post', params: { id: post.id } })">
+        <div class="title">
+          <h2>{{ post.title }}</h2>
         </div>
-        
+        <div>
+          <p>{{ post.description }}</p>
+        </div>
+        <div class="timeanddate">
+          <div class="date">{{ new Date(post.published_at).toLocaleDateString('en-GB', {
+            day: 'numeric', month: 'short', year: 'numeric'
+          }) }}</div>
+          <div>1 minute read</div>
+        </div>
+      </div>
+      <p class="edit" v-for="(post) in allPosts" @click="$router.push({ name: 'Editor', params: { id: post.id } })">Edit</p>
     </div>
-  </template>
+
+  </div>
+</template>
 
 <style scoped>
-h1{
+h1 {
   font-size: 2em;
 }
+
 h2 {
   font-size: 1.5em;
 }
 
+.date {
+  font-weight: bold;
+}
+
+.timeanddate {
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  justify-content: space-evenly;
+}
+
+.title {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.articles {
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.edit-wrapper {
+  width: 100%;
+  border: 2px solid;
+  border-radius: 5px;
+  max-width: 600px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.articleDiv {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 85%;
+  height: 120px;
+  font: 1em;
+  padding: 10px;
+}
+
+.edit{
+  cursor: pointer;
+  margin: 10px;
+  /* color: #0d82e9; */
+  color: black;
+}
 </style>
 
 
