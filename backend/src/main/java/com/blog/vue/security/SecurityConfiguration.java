43ca69277bd0936ea085bbea0228fb88bc368c20
axm2293 <axm2293@student.bham.ac.uk>
2025-12-22 22:51:40 +0000

    filters changed to set up JWT and routes cleaned up

--- backend/src/main/java/com/blog/vue/security/SecurityConfiguration.java
+++ backend/src/main/java/com/blog/vue/security/SecurityConfiguration.java
@@ -1,36 +1,49 @@
 package com.blog.vue.security;
 
+import com.blog.vue.security.jwt.AuthenticationFilter;
 import org.springframework.context.annotation.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.*;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfiguration {
+    private final AuthenticationFilter authenticationFilter;
+
+    public SecurityConfiguration(AuthenticationFilter filter) {
+        this.authenticationFilter = filter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
-                .sessionManagement(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/index.html", "/static/**", "/vite.svg", "/assets/**", "/favicon.ico").permitAll()
-                        .requestMatchers("/api/editor/**").hasRole("ADMIN")
-                        .requestMatchers("/api/posts/**", "/api/auth/login").permitAll()
-                    .anyRequest().authenticated()
-                ).build();
+                        .requestMatchers("/api/posts/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-
         return new BCryptPasswordEncoder();
     }
+
 }
 
