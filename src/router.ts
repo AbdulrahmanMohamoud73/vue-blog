58e4385fc6bc03fc6eef8900d541916d6bda3f61
axm2293 <axm2293@student.bham.ac.uk>
2025-12-24 16:36:02 +0000

    Added login component to the router as well as the authentication logic with the custom AXIOS instance

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { api } from './composables/posts'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,16 +10,54 @@ const router = createRouter({
             component: () => import('./components/Home.vue')
         },
         {
-            path: '/Post/:id',
-            name: 'Post',
+            path: '/post/:id',
+            name: 'post',
             component: () => import('./components/Post.vue')
         },
         {
-            path: '/Editor/:id',
-            name: 'Editor',
-            component: () => import('./components/Editor.vue')
+            path: '/editor/:id',
+            name: 'editor',
+            component: () => import('./components/Editor.vue'),
+            meta: { requiresAuth: true }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('./components/Login.vue')
+
         }
     ]
+})
+
+const checkAuthentication = async (): Promise<boolean> => {
+    try {
+        await api.get('auth/validate')
+        return true
+    } catch (error: any) {
+        localStorage.removeItem('authToken')
+        console.log('Login failed. Invalid or expired Token')
+        return false
+    }
+}
+
+router.beforeEach(async (to, _from, next) => {
+  if (to.meta.requiresAuth) {
+      const token = localStorage.getItem('authToken')
+
+      if (!token || token == 'undefined') {
+          return next({ path: '/login', query: { redirect: to.fullPath }});
+      }
+
+      const isAuthenticated = await checkAuthentication()
+
+      if (isAuthenticated){
+          next()
+      } else {
+          next({ path: '/login', query: { redirect: to.fullPath }})
+      }
+  } else {
+      next()
+  }
 })
 
 export default router
