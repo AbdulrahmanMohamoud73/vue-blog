15738edfe2371c154738b4e29806d46798131ac1
Abdulrahman Mohamoud <mojr1717@gmail.com>
2026-01-03 17:21:41 +0000

    Removed Header component from router view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <Header />
   <RouterView />
 </template>
 
