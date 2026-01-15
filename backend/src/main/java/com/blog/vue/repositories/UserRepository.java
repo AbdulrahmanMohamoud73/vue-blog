cfc9e8379e2d2d48f8f00379289a6a39bb2305e0
Abdulrahman Mohamoud <mojr1717@gmail.com>
2026-01-15 00:17:16 +0000

    package name amended

--- backend/src/main/java/com/blog/vue/repositories/UserRepository.java
+++ backend/src/main/java/com/blog/vue/repositories/UserRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo.repositories;
+package com.blog.vue.repositories;
 
-import com.example.demo.models.User;
+import com.blog.vue.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
