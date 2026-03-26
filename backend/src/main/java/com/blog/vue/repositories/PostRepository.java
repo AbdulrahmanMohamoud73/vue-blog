c62a88539d8bac12e346e16ecac0427b27526ab8
Abdulrahman Mohamoud <mojr1717@gmail.com>
2026-03-26 23:55:07 +0000

    PostRepository modfied to add method for fetching all posts by ID

--- backend/src/main/java/com/blog/vue/repositories/PostRepository.java
+++ backend/src/main/java/com/blog/vue/repositories/PostRepository.java
@@ -1,7 +1,10 @@
-package com.example.demo.repositories;
+package com.blog.vue.repositories;
 
-import com.example.demo.models.Post;
+import com.blog.vue.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findAllByOrderByIdAsc();
 }
